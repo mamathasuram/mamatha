2243345e59e920f51974f2ff2fd6958db1434797
mamathasuram <154117823+mamathasuram@users.noreply.github.com>
2024-01-26 15:58:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "ap-south-1a"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
